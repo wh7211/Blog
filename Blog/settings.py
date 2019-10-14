a3e796eceea40c2e83350ebd403600b88ae223ec
Tyler <wh7211@gmail.com>
2019-10-14 13:30:13 +0800

    20-2 设置博客的样式 finished

--- Blog/settings.py
+++ Blog/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 第三方应用程序
+    'bootstrap3',
+
     # 我的应用程序
     'blogs',
     'users',
@@ -126,3 +129,8 @@ STATIC_URL = '/static/'
 
 # 我的设置
 LOGIN_URL = '/users/login/'
+
+# django-bootstrap3的设置
+BOOTSTRAP3 = {
+    'include_jquery': True,
+    }
